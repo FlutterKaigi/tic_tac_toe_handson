bbc0b8ef634ecf9b86915b6a4188eae7c063ce8b
okaryo <okaryotr@gmail.com>
2023-09-07 21:06:24 +0900

    feature: 第4章の初期実装

---
+++ lib/view/board.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tic_tac_toe_handson/model/tic_tac_toe.dart';
+import 'package:tic_tac_toe_handson/provider/tic_tac_toe_provider.dart';
+
+class Board extends ConsumerWidget {
+  const Board({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ticTacToe = ref.watch(ticTacToeProvider);
+
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              _statusMessage(ticTacToe),
+              style: const TextStyle(fontSize: 24),
+            ),
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+            itemCount: 9,
+            itemBuilder: (context, index) {
+              final row = index ~/ 3;
+              final col = index % 3;
+              final mark = ticTacToe.board[row][col];
+
+              return GestureDetector(
+                onTap: () {
+                  final winner = ticTacToe.getWinner();
+                  if (mark == '' && winner == '') {
+                    ref.read(ticTacToeProvider.notifier).placeMark(row, col);
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Center(
+                    child: Text(
+                      mark,
+                      style: const TextStyle(fontSize: 32),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: ElevatedButton(
+              onPressed: () {
+                ref.read(ticTacToeProvider.notifier).resetBoard();
+              },
+              child: const Text('ゲームをリセット'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _statusMessage(TicTacToe ticTacToe) {
+    final winner = ticTacToe.getWinner();
+    final isDraw = ticTacToe.isDraw();
+
+    if (winner != '') {
+      return '$winnerの勝ち';
+    } else if (isDraw) {
+      return '引き分けです';
+    } else {
+      return '${ticTacToe.currentPlayer}の番';
+    }
+  }
+}
