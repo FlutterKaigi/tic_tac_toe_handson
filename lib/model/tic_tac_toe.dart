f11d485ad394b0b6d81726aacb9edb2839751623
okaryo <okaryotr@gmail.com>
2023-09-23 14:52:39 +0900

    第5章の初期版実装

--- lib/model/tic_tac_toe.dart
+++ lib/model/tic_tac_toe.dart
@@ -1,24 +1,68 @@
+import 'package:tic_tac_toe_handson/model/players.dart';
+
 class TicTacToe {
   final List<List<String>> board;
+  final Players players;
   final String currentPlayer;
 
-  factory TicTacToe.start() {
-    return TicTacToe._([
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ], 'X');
+  factory TicTacToe.start({
+    playerX = 'X',
+    playerO = 'O',
+  }) {
+    final players = Players(
+      playerX: playerX,
+      playerO: playerO,
+    );
+
+    return TicTacToe(
+      [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+      players,
+      players.playerX,
+    );
   }
 
-  TicTacToe._(this.board, this.currentPlayer);
+  TicTacToe(this.board, this.players, this.currentPlayer);
+
+  factory TicTacToe.fromJson(Map<String, dynamic> json) {
+    final flatBoard = List<String>.from(json['board']);
+
+    return TicTacToe(
+      [
+        List<String>.from(flatBoard.sublist(0, 3)),
+        List<String>.from(flatBoard.sublist(3, 6)),
+        List<String>.from(flatBoard.sublist(6, 9)),
+      ],
+      Players(
+        playerX: json['players']['playerX'],
+        playerO: json['players']['playerO'],
+      ),
+      json['currentPlayer'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      // Firestoreではネストした配列を扱えないため、1次元配列に変換する
+      'board': [...board[0], ...board[1], ...board[2]],
+      'players': {
+        'playerX': players.playerX,
+        'playerO': players.playerO,
+      },
+      'currentPlayer': currentPlayer,
+    };
+  }
 
   TicTacToe placeMark(int row, int col) {
     if (board[row][col].isEmpty) {
       final newBoard = List.of(board);
       newBoard[row][col] = currentPlayer;
-      String nextPlayer = currentPlayer == 'X' ? 'O' : 'X';
+      String nextPlayer = currentPlayer == players.playerX ? players.playerO : players.playerX;
 
-      return TicTacToe._(newBoard, nextPlayer);
+      return TicTacToe(newBoard, players, nextPlayer);
     }
     return this;
   }
@@ -50,6 +94,6 @@ class TicTacToe {
   }
 
   TicTacToe resetBoard() {
-    return TicTacToe.start();
+    return TicTacToe.start(playerX: players.playerX, playerO: players.playerO);
   }
 }
--- lib/provider/tic_tac_toe_provider.dart
+++ lib/provider/tic_tac_toe_provider.dart
@@ -1,18 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tic_tac_toe_handson/model/tic_tac_toe.dart';
+import 'package:tic_tac_toe_handson/repository/tic_tac_toe_repository.dart';
 
-final ticTacToeProvider = StateNotifierProvider.autoDispose<TicTacToeProvider, TicTacToe>((ref) {
-  return TicTacToeProvider();
+final ticTacToeProvider = StreamProvider.autoDispose<TicTacToe>((ref) {
+  // 対戦相手同士のIDを設定する
+  return getTicTacToe(playerX: 'flutter', playerO: 'kaigi');
 });
-
-class TicTacToeProvider extends StateNotifier<TicTacToe> {
-  TicTacToeProvider() : super(TicTacToe.start());
-
-  placeMark(int row, int col) {
-    state = state.placeMark(row, col);
-  }
-
-  resetBoard() {
-    state = state.resetBoard();
-  }
-}
