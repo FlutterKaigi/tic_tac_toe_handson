5cea3f3485a104f5882385137a81f5e12a3e562e
okaryo <okaryotr@gmail.com>
2023-09-24 16:23:12 +0900

    第2章の初期版実装

---
+++ lib/model/tic_tac_toe.dart
@@ -0,0 +1,70 @@
+import 'package:tic_tac_toe_handson/model/players.dart';
+
+class TicTacToe {
+  final List<List<String>> board;
+  final Players players;
+  final String currentPlayer;
+
+  factory TicTacToe.start({
+    playerX = 'X',
+    playerO = 'O',
+  }) {
+    final players = Players(
+      playerX: playerX,
+      playerO: playerO,
+    );
+
+    return TicTacToe(
+      [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+      players,
+      players.playerX,
+    );
+  }
+
+  TicTacToe(this.board, this.players, this.currentPlayer);
+
+  TicTacToe placeMark(int row, int col) {
+    if (board[row][col].isEmpty) {
+      final newBoard = List.of(board);
+      newBoard[row][col] = currentPlayer;
+      String nextPlayer = currentPlayer == players.playerX ? players.playerO : players.playerX;
+
+      return TicTacToe(newBoard, players, nextPlayer);
+    }
+    return this;
+  }
+
+  String getWinner() {
+    for (int i = 0; i < 3; i++) {
+      // row = i における横の判定
+      if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0].isNotEmpty) {
+        return board[i][0];
+      }
+      // col = i における縦の判定
+      if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i].isNotEmpty) {
+        return board[0][i];
+      }
+    }
+    // 左上から右下への斜めの判定
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0].isNotEmpty) {
+      return board[0][0];
+    }
+    // 右上から左下への斜めの判定
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2].isNotEmpty) {
+      return board[0][2];
+    }
+    return '';
+  }
+
+  bool isDraw() {
+    return getWinner().isEmpty && board.every((row) => row.every((cell) => cell.isNotEmpty));
+  }
+
+  TicTacToe resetBoard() {
+    return TicTacToe.start(playerX: players.playerX, playerO: players.playerO);
+  }
+}
