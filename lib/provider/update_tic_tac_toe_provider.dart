ae07dffa821dfd75fccfef9a0329b289b190cb48
Daichi Aoki <fremie.off@gmail.com>
2023-10-01 23:56:16 +0900

    :sparkles: updateをProvider化

--- lib/provider/update_tic_tac_toe_provider.dart
+++ lib/provider/update_tic_tac_toe_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tic_tac_toe_handson/model/tic_tac_toe.dart';
 import 'package:tic_tac_toe_handson/repository/tic_tac_toe_repository.dart';
 
-final ticTacToeProvider = StreamProvider.autoDispose<TicTacToe>((ref) {
-  // 対戦相手同士のIDを設定する
-  return getTicTacToe(playerX: 'flutter', playerO: 'kaigi');
-});
+final updateTicTacToeProvider =
+    AutoDisposeFutureProviderFamily<void, TicTacToe>(
+  (ref, arg) => ref.watch(ticTacToeRepositoryProvider).update(arg),
+);
