a62fdb4a863448c974b2054ee539c3f7a5f2665a
Daichi Aoki <fremie.off@gmail.com>
2023-10-01 23:54:21 +0900

    :+1: リポジトリをクラス＆Provider化（機能変更なし）

--- lib/repository/tic_tac_toe_repository.dart
+++ lib/repository/tic_tac_toe_repository.dart
@@ -1,26 +1,48 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tic_tac_toe_handson/model/tic_tac_toe.dart';
 
-final client = FirebaseFirestore.instance;
-const rootCollectionKey = 'tic_tac_toe';
+final ticTacToeRepositoryProvider = AutoDisposeProvider<TicTacToeRepository>(
+  (ref) => TicTacToeRepository(),
+);
 
-Stream<TicTacToe> getTicTacToe({String playerX = 'X', String playerO = 'O'}) {
-  final documentKey = _documentKey(playerX, playerO);
-  return client.collection(rootCollectionKey).doc(documentKey).snapshots().map((snapshot) {
-    final data = snapshot.data();
-    if (data == null) {
-      return TicTacToe.start(playerX: playerX, playerO: playerO);
-    }
+/// 盤面のデータを管理するリポジトリ
+final class TicTacToeRepository {
+  TicTacToeRepository();
 
-    return TicTacToe.fromJson(data);
-  });
-}
+  final _client = FirebaseFirestore.instance;
+  static const _collectionKey = 'tic_tac_toe';
 
-Future<void> updateTicTacToe(TicTacToe ticTacToe) async {
-  final documentKey = _documentKey(ticTacToe.players.playerX, ticTacToe.players.playerO);
-  await client.collection(rootCollectionKey).doc(documentKey).set(ticTacToe.toJson());
-}
+  String _documentKey(String playerX, String playerO) {
+    return '${playerX}_$playerO';
+  }
 
-String _documentKey(String playerX, String playerO) {
-  return '${playerX}_${playerO}';
+  CollectionReference<TicTacToe> _colRef() =>
+      _client.collection(_collectionKey).withConverter(
+            fromFirestore: (doc, _) => TicTacToe.fromJson(doc.data()!),
+            toFirestore: (entity, _) => entity.toJson(),
+          );
+
+  /// 盤面のデータを取得する
+  Stream<TicTacToe> get({
+    String playerX = 'X',
+    String playerO = 'O',
+  }) {
+    final documentKey = _documentKey(playerX, playerO);
+    return _colRef().doc(documentKey).snapshots().map(
+          (e) =>
+              e.data() ??
+              TicTacToe.start(
+                playerX: playerX,
+                playerO: playerO,
+              ),
+        );
+  }
+
+  /// 盤面のデータを更新する
+  Future<void> update(TicTacToe ticTacToe) async {
+    final documentKey =
+        _documentKey(ticTacToe.players.playerX, ticTacToe.players.playerO);
+    await _colRef().doc(documentKey).set(ticTacToe);
+  }
 }
