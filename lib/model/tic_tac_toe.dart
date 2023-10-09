ee27831c686688fd4659517a915bbdebfbb70da7
okaryo <okaryotr@gmail.com>
2023-10-09 17:17:00 +0900

    modify: 盤上の印をO/Xで表現し、勝者をIDで表示するよう修正

--- lib/model/tic_tac_toe.dart
+++ lib/model/tic_tac_toe.dart
@@ -30,7 +30,7 @@ class TicTacToe {
   TicTacToe placeMark(int row, int col) {
     if (board[row][col].isEmpty) {
       final newBoard = List.of(board);
-      newBoard[row][col] = currentPlayer;
+      newBoard[row][col] = currentPlayer == players.playerX ? 'X' : 'O';
       String nextPlayer = currentPlayer == players.playerX ? players.playerO : players.playerX;
 
       return TicTacToe(newBoard, players, nextPlayer);
@@ -42,20 +42,20 @@ class TicTacToe {
     for (int i = 0; i < 3; i++) {
       // row = i における横の判定
       if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0].isNotEmpty) {
-        return board[i][0];
+        return board[i][0] == 'X' ? players.playerX : players.playerO;
       }
       // col = i における縦の判定
       if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i].isNotEmpty) {
-        return board[0][i];
+        return board[0][i] == 'X' ? players.playerX : players.playerO;
       }
     }
     // 左上から右下への斜めの判定
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0].isNotEmpty) {
-      return board[0][0];
+      return board[0][0] == 'X' ? players.playerX : players.playerO;
     }
     // 右上から左下への斜めの判定
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2].isNotEmpty) {
-      return board[0][2];
+      return board[0][2] == 'X' ? players.playerX : players.playerO;
     }
     return '';
   }
